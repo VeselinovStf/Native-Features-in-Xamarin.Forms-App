9094aada9dc40652653d57ff2c4ad182f7a7a198
s v <chofexx@gmail.com>
2020-11-16 18:16:08 +0200

    Adding Network Access Display Messge

--- src/PhotoShare/PhotoShare/AppShell.xaml.cs
+++ src/PhotoShare/PhotoShare/AppShell.xaml.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace PhotoShare
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
+        NetworkAccess LastNetworkAccess = NetworkAccess.Unknown;
+
         public AppShell()
         {
             InitializeComponent();
+
+            Connectivity.ConnectivityChanged += Conectivity_Changed;
+        }
+
+        private async void Conectivity_Changed(object sender, ConnectivityChangedEventArgs e)
+        {
+            var access = e.NetworkAccess;
+
+            if (access != LastNetworkAccess)
+            {
+                LastNetworkAccess = access;
+
+                if (access == NetworkAccess.Internet)
+                {
+                    await App.Current.MainPage.DisplayAlert("Network Access", "Internet access availible", "Ok");
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Network Access", "Internet access unavailible", "Ok");
+                }
+            }
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
